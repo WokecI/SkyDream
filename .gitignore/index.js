d384dde048242f57e04ba94dbb7c93697cffa716
SkylostFR <45940771+SkylostFR@users.noreply.github.com>
2019-01-12 17:25:00 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -55,7 +55,18 @@ bot.on('message', message => {
             .setDescription(':cry: **' + member.user.username + '** a quitté ' + member.guild.name)
             .setFooter('Nous sommes désormais ' + member.guild.memberCount)
             .setColor('RANDOM')
-        member.guild.channels.get('524172484879187968').send(embed2)
-    }
+        member.guild.channels.get('524172484879187968').send(embed2);
+    },
 
-    )})
+    bot.on("message", message => {
+        if (!message.guild) return
+        let args = message.content.trim().split(/ +/g)
+     
+        if (args[0].toLowerCase() === prefix + "clear") {
+            if (!message.member.hasPermission('MANAGE_MESSAGES')) return message.channel.send("Vous n'avez pas la permission d'utiliser cette commande")
+            let count = args[1]
+            if (!count) return message.channel.send("Veuillez indiquer un nombre de messages à supprimer")
+            if (isNaN(count)) return message.channel.send("Veuillez indiquer un nombre valide")
+            if (count < 1 || count > 100) return message.channel.send("Veuillez indiquer un nombre entre 1 et 100")
+            message.channel.bulkDelete(parseInt(count) + 1)
+        }}))})
