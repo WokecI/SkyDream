f43cef25bd2147a360742b7ce7c23532ac1fe4d7
SkylostFR <45940771+SkylostFR@users.noreply.github.com>
2019-01-12 23:27:58 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -17,7 +17,6 @@ bot.on('message', message => {
             .addField("help","Affiche ce message.")
             .addField("liens","Donne des liens utiles ^^.")
             .addField("discordinfos","Donne les informations du serveur.")
-            .addField("sondage","permet de crée un sondage.")
             .addField("helpmod","Affiche les commandes de modérations.")
             .setColor("RANDOM")
             .setFooter("Commandes faites par: Skylost#5655")
@@ -76,49 +75,3 @@ bot.on('message', message => {
         .setFooter("Commandes faites par: Skylost#5655")
         message.channel.sendEmbed(infos);
     }});
-
-bot.on('message', message => {
-    if (message.content.startsWith(prefix + "sondage")) {
-        let args = message.content.split(" ").slice(1);
-        let thingToEcho = arg.join(" ")
-        var sondage = new Discord.RichEmbed()
-            .setDescription("Sondage")
-            .addField(thingToEcho, "Répondre avec :white_check_mark: ou :x:")
-            .setColor("RANDOM")
-            .setTimestamp()
-        message.channel.sendEmbed(sondage)
-        .then(function (message) { 
-            message.react("❌")
-            message.react("✅")
-        }).catch(function() {
-        });
-    }else{
-        return message.reply("Tu n'as pas la permission.")
-}},)
-
-    switch (args[0].toLowerCase()) {
-        case "roll":
-            var roll = Math.floor(Math.random() * args[1]) +1;
-            if (!roll) return message.reply("Entre un numéro")
-            message.channel.send("Je choisis le numéro " + roll + " !");
-            break;
-        case "8ball":
-        let args = message.content.split(" ").slice(1);
-        let tte = args.join(" ")
-        if (!tte) {
-            return message.reply("Merci de poser une question :8ball:")};
-
-            var replys = [
-                "Oui",
-                "Nan",
-                "Je sais pas",
-                "Peut être",
-            ];
-
-            let reponse = (replys[Math.floor(Math.random() * replys.length)])
-            var bembed = new Discord.RichEmbed()
-            .setDescription(":8ball: 8ball")
-            .addField("Question", tte)
-            .addField("Réponse", reponse)
-            message.channel.sendEmbed(bembed)
-    }
