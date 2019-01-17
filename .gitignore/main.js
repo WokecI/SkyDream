d7503c2cabc8dbc65264db117d00ba3bd893c1e2
SkylostFR <45940771+SkylostFR@users.noreply.github.com>
2019-01-18 00:32:12 +0100

    Update main.js

--- .gitignore/main.js
+++ .gitignore/main.js
@@ -5,15 +5,15 @@ const bot = new Discord.Client({disableEveryone: true});
 
 bot.on("ready", async () => {
     console.log(`${bot.user.username} est en ligne !`);
-    bot.user.setActivity(`.help sur ${bot.guilds.size} serveurs`);
+    bot.user.setActivity(`.help|${bot.guilds.size} serveurs|${bot.users.size} users`);
 });
 
 bot.on("guildCreate", guild => {
-    bot.user.setActivity(`.help sur ${bot.guilds.size} serveurs`);
+    bot.user.setActivity(`.help|${bot.guilds.size} serveurs|${bot.users.size} users`);
     });
     
 bot.on("guildDelete", guild => {
-    bot.user.setActivity(`.help sur ${bot.guilds.size} serveurs`);
+    bot.user.setActivity(`.help|${bot.guilds.size} serveurs|${bot.users.size} users`);
     });
 
 bot.on('message', async message => {
